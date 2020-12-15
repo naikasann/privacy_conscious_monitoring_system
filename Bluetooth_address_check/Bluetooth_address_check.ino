6f8cca7910ba993495435a3d8af982c438377470
Yamazaki Kohei <51977859+naikasann@users.noreply.github.com>
2020-12-16 03:43:11 +0900

    :zap: 正しく動作するように

--- Bluetooth_address_check/Bluetooth_address_check.ino
+++ Bluetooth_address_check/Bluetooth_address_check.ino
@@ -2,7 +2,9 @@
 #include <BLEDevice.h>
 
 #define SCANTIME 1
-const char* uuid = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"; // Tomoru Service UUID
+
+BLEScan* pBLEScan;
+const char* uuid = "b3b36901-50d3-4044-808d-50835b13a6cd"; // Tomoru Service UUID
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -10,39 +12,21 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         if(advertisedDevice.haveServiceUUID()){
             // Find Tomoru's service UUID.If there is more than one, there will be more than one.
             if(strncmp(advertisedDevice.getServiceUUID().toString().c_str(),uuid, 36) == 0){
-                Serial.println(advertisedDevice.getAddress().toString().c_str())
+                Serial.println(advertisedDevice.getAddress().toString().c_str());
             }
         }
     }
 };
 
-// function :: If it is different from the previous state, send data via Sigfox.
-//             (Data will be sent at regular intervals while you are out).
-void Task1(void *pvParameters){
-    // Prepare for BLEscan.
-    BLEDevice::init("");
-    BLEScan* pBLEScan = BLEDevice::getScan();
-    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    while(1){
-        Serial.println("check beacon.");
-        // Perform BLE search.
-        pBLEScan->setActiveScan(true);
-        pBLEScan->start(SCANTIME, false);
-        pBLEScan->clearResults(); // Reset the BLE results.
-        // Take a break from exploring the BLE.
-        delay(CHECK_DELAY * 1000);
-    }
-}
-
 void setup() {
     // for Atom matrix and BLE device.
     M5.begin(true, false, true);
-    BLEDevice::init("");
-    BLEScan* pBLEScan = BLEDevice::getScan();
-    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
 }
 
 void loop(){
+    BLEDevice::init("");
+    pBLEScan = BLEDevice::getScan();
+    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
     pBLEScan->setActiveScan(true);
     pBLEScan->start(SCANTIME, false);
     pBLEScan->clearResults(); // Reset the BLE results.
